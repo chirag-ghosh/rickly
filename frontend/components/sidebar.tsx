a5f31c716de772165dfac21279b46a54caf0717f
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-22 21:16:11 +0530

    Added sidebar;

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -1,5 +1,6 @@
 import { NextComponentType } from "next";
 import Link from "next/link";
+import { BarChart2, Star, User, Users } from "react-feather";
 
 const Sidebar: NextComponentType = () => {
     return(
@@ -8,11 +9,31 @@ const Sidebar: NextComponentType = () => {
                 <img src="/logo.png" alt="rickly-logo"></img>
                 <div>RICKLY</div>
             </div>
-            <div className="options">
-                <Link href="/team">Teams</Link>
-                <Link href="/player">Players</Link>
-                <Link href="/stats">Statistics</Link>
-                <Link href="/tournament">Tournament</Link>
+            <div className="option-list">
+                <Link href="/team">
+                    <div className="option">
+                        <Users size={35} />
+                        <div> Team</div>
+                    </div>
+                </Link>
+                <Link href="/player">
+                    <div className="option">
+                        <User size={35} />
+                        <div> Players</div>
+                    </div>
+                </Link>
+                <Link href="/stats">
+                    <div className="option">
+                        <BarChart2 size={35} />
+                        <div> Statistics</div>
+                    </div>
+                </Link>
+                <Link href="/tournament">
+                    <div className="option">
+                        <Star size={35} />
+                        <div> Tournament</div>
+                    </div>
+                </Link>
             </div>
         </div>
     )
