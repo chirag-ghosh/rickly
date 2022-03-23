fe9fe048132c47df0790a5abf0c0edf342a02dbc
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-24 01:56:07 +0530

    Made necessary changes;

---
+++ frontend/pages/stats/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const Stats: NextPage = () => {
+    return(
+        <div>Stats page</div>
+    )
+}
+
+export default Stats;
---
+++ frontend/pages/team/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const Team: NextPage = () => {
+    return(
+        <div>Team page</div>
+    )
+}
+
+export default Team;
