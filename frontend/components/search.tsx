218177faaffed5ac622d808f6e102d594140112f
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-23 19:53:31 +0530

    Made a semi-working topbar;

--- frontend/components/search.tsx
+++ frontend/components/search.tsx
@@ -7,6 +7,12 @@ const Search: NextComponentType = () => {
 
     const { query, setQuery, teams, players} = useSearchContext();
 
+    const searchClickHandler = (type: 'team' | 'player', item: string) => {
+        setQuery("");
+        console.log(item);
+        //move to corresponding section
+    }
+
     return(
         <div className="search">
             <input
@@ -16,6 +22,32 @@ const Search: NextComponentType = () => {
                 onChange={(event) => setQuery(event.target.value)}
             />
             <div className='icon'><SearchIcon /></div>
+            {query !== "" && (
+                <div className='dropdown'>
+                    <div className='dropdown-label'>Teams</div>
+                    <ul>
+                        {teams.map((team, index) => {
+                            return(
+                                <li key={index} onClick={() => searchClickHandler('team', team.name)}>{team.name}</li>
+                            )
+                        })}
+                        {teams.length === 0 && (
+                            <div>No results</div>
+                        )}
+                    </ul>
+                    <div className='dropdown-label'>Players</div>
+                    <ul>
+                        {players.map((player, index) => {
+                            return(
+                                <li key={index} onClick={() => searchClickHandler('player', player.name)}>{player.name}</li>
+                            )
+                        })}
+                        {players.length === 0 && (
+                            <div>No results</div>
+                        )}
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
