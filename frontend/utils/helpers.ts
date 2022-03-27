e3d9c7a81aa74d42340e779a211f8445a536e444
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-27 12:58:13 +0530

    Made individual player page;

--- frontend/utils/helpers.ts
+++ frontend/utils/helpers.ts
@@ -1,5 +1,5 @@
-import { Team } from "../types";
-import { sampleTeams } from "./sampleData";
+import { Player, Team } from "../types";
+import { samplePlayers, sampleTeams } from "./sampleData";
 
 export const getTeamFromID = (id: string | string[] | undefined): Team | null => {
 
@@ -7,6 +7,16 @@ export const getTeamFromID = (id: string | string[] | undefined): Team | null =>
     const teamList = sampleTeams;
     for(var i = 0; i < teamList.length; i++) {
         if(teamList[i].uuid === id) return teamList[i];
+    }
+    return null;
+}
+
+export const getPlayerFromID = (id: string | string[] | undefined): Player | null => {
+
+    if(id === undefined) return null;
+    const playerList = samplePlayers;
+    for(var i = 0; i < playerList.length; i++) {
+        if(playerList[i].uuid === id) return playerList[i];
     }
     return null;
 }
