672af6f30c5fb3a2747ca8f1f1c714969cb70fc7
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-27 10:33:56 +0530

    Showing players list;

--- frontend/pages/player/index.tsx
+++ frontend/pages/player/index.tsx
@@ -1,8 +1,34 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useSearchContext } from "../../hooks/useSearchContext";
+import { Player, Team } from "../../types";
+import { sampleTeams } from "../../utils/sampleData";
 
 const Player: NextPage = () => {
+
+    const [playersList, setPlayersList] = useState<Player[]>([]);
+
+    const {players} = useSearchContext();
+    const router = useRouter();
+
+    useEffect(() => {
+        setPlayersList([...players, ...players, ...players, ...players, ...players, ...players, ...players, ...players])
+    })
+
     return(
-        <div>Player page</div>
+        <div className="players-page">
+            <h1>List of all players</h1>
+            <div className="player-list">
+                {
+                    playersList.map((player) => {
+                        return(
+                            <div onClick={() => router.push(`player/${player.uuid}`)} className="player">{player.name}</div>
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
