d21bc04d75c6bd04037b802ca285d9fb56e12114
Aritra Mitra <84005101+Ammetre@users.noreply.github.com>
2022-03-30 08:59:00 +0530

    Deleted whole backend;

--- backend/team/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("The Teams Page is here")
-def match(request,match_id):
-    return render(request,'team/index.html',{"match_id": match_id})
