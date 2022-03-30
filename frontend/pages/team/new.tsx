0e17741a4b47656e10613312d004f8793d4ca2bb
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-30 23:56:31 +0530

    Did something(4);

--- frontend/pages/team/new.tsx
+++ frontend/pages/team/new.tsx
@@ -1,22 +1,35 @@
+import axios from "axios";
+import { useRouter } from "next/router";
 import { useState } from "react";
+import { BACKEND_URL } from "../../utils/constants";
 
 const NewTeam = () => {
 
     const [name, setName] = useState<string>('');
 
+    const router = useRouter();
+
+    const createTeam = () => {
+        axios.post(`${BACKEND_URL}/teams`, {name: name})
+            .then((response) => {
+                router.replace('/teams')
+            })
+            .catch((err) => console.log(err));
+    }
+
     return(
         <div className="new-team-page">
             <h1>Create a new team here <span>( Note that players will be taken randomly from a pool of uncapped players )</span></h1>
             <div className="division">
-                <div className="divided-block">
+                {/* <div className="divided-block">
                     <button>Generate a team with random name</button>
                 </div>
                 <div className="divider">
                     OR
-                </div>
+                </div> */}
                 <div className="divided-block">
                     <input type='text' placeholder="Enter team name" onChange={(event) => setName(event.target.value)}></input>
-                    <button>Create team</button>
+                    <button onClick={() => createTeam()}>Create team</button>
                 </div>
             </div>
         </div>
