bf13272a768ccdc55e9628ffd9a57941abda1c45
“chirag-ghosh” <cghosh828049@gmail.com>
2022-03-30 22:48:31 +0530

    Showing player stats;

--- frontend/pages/stats/player/[id].tsx
+++ frontend/pages/stats/player/[id].tsx
@@ -1,31 +1,20 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useSearchContext } from "../../../hooks/useSearchContext";
+import { Player } from "../../../types";
 import { getPlayerFromID } from "../../../utils/helpers";
 
 const Player = () => {
 
     const {playerList} = useSearchContext();
     const router = useRouter();
-    const player = getPlayerFromID(router.query.id, playerList);
 
-    const [matchCount, setMatchCount] = useState<number>();
-    const [totalRuns, setTotalRuns] = useState<number>();
-    const [totalBalls, setTotalBalls] = useState<number>();
-    const [runsGiven, setRunsGiven] = useState<number>();
-    const [ballsDelivered, setBallsDelivered] = useState<number>();
-    const [wicketsTaken, setWicketsTaken] = useState<number>();
+    const [player, setPlayer] = useState<any>();
 
     useEffect(() => {
 
-        setMatchCount(5);
-        setTotalRuns(240);
-        setTotalBalls(200);
-
-        setRunsGiven(50);
-        setBallsDelivered(30);
-        setWicketsTaken(1);
-    }, []);
+        setPlayer(getPlayerFromID(router.query.id, playerList));
+    }, [playerList]);
 
     return(
         <div className="player-page">
@@ -36,46 +25,38 @@ const Player = () => {
                     <div>{player?.team}</div>
                 </div>
                 <div className="player-highlight green">
-                    <h2>Matches played</h2>
-                    <div>{matchCount}</div>
+                    <h2>Age</h2>
+                    <div>{player?.age}</div>
                 </div>
             </div>
             <h2>Batter stats</h2>
             <div className="player-stats-highlight-list">
                 <div className="player-highlight blue">
                     <h2>Runs Scored</h2>
-                    <div>{totalRuns}</div>
+                    <div>{player?.runcount}</div>
                 </div>
                 <div className="player-highlight green">
-                    <h2>Balls played</h2>
-                    <div>{totalBalls}</div>
+                    <h2>Half Centuries</h2>
+                    <div>{player?._50count}</div>
                 </div>
                 <div className="player-highlight blue">
-                    <h2>Average</h2>
-                    <div>{matchCount !== undefined && matchCount !== 0 && totalRuns !== undefined ? (totalRuns/matchCount) : '-'}</div>
-                </div>
-                <div className="player-highlight red">
-                    <h2>Strike Rate</h2>
-                    <div>{totalBalls !== undefined && totalBalls !== 0 && totalRuns !== undefined ? (totalRuns/totalBalls * 100) : '-'}</div>
+                    <h2>Centuries</h2>
+                    <div>{player?._1Ccount}</div>
                 </div>
             </div>
             <h2>Bowler stats</h2>
             <div className="player-stats-highlight-list">
                 <div className="player-highlight blue">
-                    <h2>Balls delivered</h2>
-                    <div>{totalBalls}</div>
+                    <h2>Wickets Taken</h2>
+                    <div>{player?.wickount}</div>
                 </div>
                 <div className="player-highlight green">
-                    <h2>Runs given</h2>
-                    <div>{totalRuns}</div>
+                    <h2>5-wickets</h2>
+                    <div>{player?._5wcount}</div>
                 </div>
                 <div className="player-highlight blue">
-                    <h2>Wickets Taken</h2>
-                    <div>{wicketsTaken}</div>
-                </div>
-                <div className="player-highlight red">
-                    <h2>Economy</h2>
-                    <div>{ballsDelivered !== undefined && ballsDelivered !== 0 && runsGiven !== undefined ? (runsGiven/ballsDelivered * 100) : '-'}</div>
+                    <h2>Catches Taken</h2>
+                    <div>{player?.catcount}</div>
                 </div>
             </div>
         </div>
