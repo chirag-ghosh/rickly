a5b6230de2ffa661250fe76f5fac0d902ae9e13a
Aritra Mitra <84005101+Ammetre@users.noreply.github.com>
2022-03-30 10:30:47 +0530

    Added urls;

--- backend/team/urls.py
+++ backend/team/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path('tournaments/unscheduled/<int:id>/',views.TournamentUView.as_view()),
     path('tournaments/scheduled/',views.TournamentSView.as_view()),
     path('tournaments/scheduled/<int:id>/',views.TournamentSView.as_view()),
+    path('tournaments/completed/',views.TournamentCView.as_view()),
+    path('tournaments/completed/<int:id>/',views.TournamentCView.as_view()),
+    path('teams/',views.TeamView.as_view()),
+    path('teams/<int:id>/',views.TeamView.as_view()),
+    path('players/',views.PlayerView.as_view()),
+    path('players/<int:id>/',views.PlayerView.as_view()),
+    path('matches/',views.MatchView.as_view()),
+    path('matches/<int:id>/',views.MatchView.as_view()),
+    
 ]
